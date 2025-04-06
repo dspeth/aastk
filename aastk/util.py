3d0c46d43fa3c868c5a998d243898fc20e8a2ab4
ewearpel <ewearpel@gmail.com>
2025-04-06 17:20:37 +0200

    moved utility functions for file writing from pasr to util

--- aastk/util.py
+++ aastk/util.py
@@ -34,3 +34,70 @@ def determine_file_type(file_path):
             return "fastq"
         else:
             raise ValueError(f"Unrecognized file type in {file_path}")
+
+def write_fa_matches(seq_file, ids):
+    """
+    Generator function to process FASTA file and yield matching sequences in fasta format.
+
+    Args:
+    - seq_file: Path to the fasta file containing the sequences to search.
+    - ids: set of ids to retrieve matches for.
+
+    Yields:
+    - Header and sequence of matching sequences.
+    """
+    matching = False
+    sequence = ""
+
+    ## parser can also be in util.py
+    with open(seq_file, 'r') as sf:
+        for line in sf:
+            line = line.strip()
+            if line.startswith(">"):
+                if matching:
+                    yield (header, sequence)
+                sequence = ""
+                seq_id = line.split()[0][1:]  # Get the query ID without '>'
+                if seq_id in ids:
+                    matching = True
+                    header = line
+                else:
+                    matching = False
+            elif matching:
+                sequence += line
+
+        if matching:
+            yield (header, sequence)
+
+
+def write_fq_matches(seq_file, ids):
+    """
+    Generator function to process FASTQ file and yield matching sequences in fasta format.
+
+    Args:
+    - seq_file: Path to the fastq file containing the sequences to search.
+    - ids: set of ids to retrieve matches for.
+
+    Yields:
+    - Header and sequence of matching sequences (converted to fasta format).
+    """
+    matching = False
+    line_count = 0
+    sequence = ""
+
+    with open(seq_file, 'r') as sf:
+        for line in sf:
+            line = line.strip()
+            line_count += 1
+
+            if line_count == 1:
+                seq_id = line.split()[0][1:]  # Get the query ID without '@'
+                matching = seq_id in ids
+                if matching:
+                    header = seq_id  # Store the fastq ID to convert to fasta format
+            elif line_count == 2 and matching:
+                sequence = line  # Store the sequence for matching read
+            elif line_count == 4:
+                line_count = 0  # Reset after each fastq record
+                if matching:
+                    yield (header, sequence)
